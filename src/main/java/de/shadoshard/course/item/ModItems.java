8e2bdf29803fabddf3c72d8ebf5e9d57e911c8b7
ShadoShard <benediktmerk.pc@gmail.com>
2024-10-02 17:32:02 +0200

    Commit

--- src/main/java/de/shadoshard/course/item/ModItems.java
+++ src/main/java/de/shadoshard/course/item/ModItems.java
@@ -1,6 +1,7 @@
 package de.shadoshard.course.item;
 
 import de.shadoshard.course.Course;
+import de.shadoshard.course.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,9 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(512)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
